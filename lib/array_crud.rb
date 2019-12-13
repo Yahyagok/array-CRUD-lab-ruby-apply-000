3db72ca27543b70894d1c93ff1e4532d22c6dbaa
Yahya  Gok <ygok17@hotmail.com>
2019-12-13 20:16:03 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -9,7 +9,9 @@ end
 def add_element_to_end_of_array(array, element)
   array.push(element)
 end
-
+def add_element_to_start_of_array(array,element)
+  array.unshift
+end
 def remove_element_from_end_of_array(array)
   array.pop
 end
